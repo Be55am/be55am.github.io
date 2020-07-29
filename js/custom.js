ed92fd84af87e02fe51b38d6feb5897ff386aa03
Bessam Sahli <bsahli@wissal-sys.com>
2020-07-29 12:37:04 +0100

    test commit 2

--- js/custom.js
+++ js/custom.js
@@ -57,6 +57,7 @@ $(function(){
         fetch('https://0d1xua7618.execute-api.us-east-1.amazonaws.com/default/mail-sender', {
 
 
+
             method : 'post',
             mode: 'no-cors',
             body: JSON.stringify(object)
